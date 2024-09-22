010ed1f769dc830550b7e04fc849a2dc4c9181ca
AniaPiWo <snowflake@wp.pl>
2024-09-22 11:43:21 +0200

    init: remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,14 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return <main className="flex h-screen items-center justify-center">app</main>;
+}
+
+//const resources = [];
