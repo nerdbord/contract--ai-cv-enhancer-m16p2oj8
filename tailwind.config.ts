dd525f31479d290f614055653aaeb2efeaefca9d
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-10-03 03:00:26 +0200

    tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import tailwindcssSignals from 'tailwindcss-signals';
+
+const config: Config = {
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {
+     
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('children', '&>*');
+    }),
+    tailwindcssSignals,
+  ],
+};
+export default config;
