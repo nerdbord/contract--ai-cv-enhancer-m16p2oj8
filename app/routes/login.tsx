240b2cc4351e897c6e9fe886c3160c70aa86b528
filwas <fwas.corp@gmail.com>
2024-10-04 21:25:27 +0200

    login page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -9,11 +9,17 @@ import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { verifyLogin } from "~/models/user.server";
 import { createUserSession, getUserId } from "~/session.server";
 import { validateEmail } from "~/utils";
+import { Button } from "~/components/ui/button";
+
+import logo from '../../public/logo.svg'
+import triangles from '../../public/login triangles.svg'
 
 export const meta: MetaFunction = () => {
-  return [{
-    title: "Login",
-  }];
+  return [
+    {
+      title: "Login",
+    },
+  ];
 };
 
 interface ActionData {
@@ -43,14 +49,14 @@ export const action: ActionFunction = async ({ request }) => {
   if (typeof password !== "string") {
     return json(
       { errors: { password: "Valid password is required." } },
-      { status: 400 }
+      { status: 400 },
     );
   }
 
   if (password.length < 6) {
     return json(
       { errors: { password: "Password is too short" } },
-      { status: 400 }
+      { status: 400 },
     );
   }
 
@@ -59,7 +65,7 @@ export const action: ActionFunction = async ({ request }) => {
   if (!user) {
     return json(
       { errors: { email: "Invalid email or password" } },
-      { status: 400 }
+      { status: 400 },
     );
   }
 
@@ -90,12 +96,23 @@ export default function Login() {
   }, [actionData]);
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        <Form method="post" className="space-y-6" noValidate>
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="w-full max-w-2xl p-8">
+        <img
+          src={logo}
+          alt="Logo"
+          className="absolute top-16 left-24"
+        />
+
+        <Form
+          method="post"
+          className="mt-34 ml-52 space-y-6 max-w-md"
+          noValidate
+        >
+          <h2 className="text-center text-2xl font-semibold">Log In</h2>
           <div>
             <label className="text-sm font-medium" htmlFor="email">
-              <span className="block text-gray-700">Email Address</span>
+              <span className="block text-gray-900 ">Email</span>
               {actionData?.errors?.email && (
                 <span className="block pt-1 text-red-700" id="email-error">
                   {actionData?.errors?.email}
@@ -103,8 +120,9 @@ export default function Login() {
               )}
             </label>
             <input
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              className="w-full rounded-md border border-gray-400 px-3 py-2 my-1.5 text-lg"
               autoComplete="email"
+              placeholder="Email"
               type="email"
               name="email"
               id="email"
@@ -115,10 +133,7 @@ export default function Login() {
           </div>
           <div>
             <label className="text-sm font-medium" htmlFor="password">
-              <span className="block text-gray-700">Password</span>
-              <span className="block font-light text-gray-700">
-                Must have at least 6 characters.
-              </span>
+              <span className="block text-gray-900">Password</span>
               {actionData?.errors?.password && (
                 <span className="pt-1 text-red-700" id="password-error">
                   {actionData?.errors?.password}
@@ -128,47 +143,38 @@ export default function Login() {
             <input
               id="password"
               type="password"
+              placeholder="Password"
               name="password"
               autoComplete=""
-              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              className="w-full rounded-md border border-gray-400 px-3 py-2 my-1.5 text-lg"
               aria-invalid={actionData?.errors?.password ? true : undefined}
               aria-describedby="password-error"
               ref={passwordRef}
             />
           </div>
-          <button
-            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+          <Button
+            className="w-full rounded bg-cyan-900 py-2 px-4 text-white hover:bg-cyan-950 focus:bg-cyan-700"
             type="submit"
           >
             Log in
-          </button>
+          </Button>
           <input type="hidden" name="redirectTo" value={redirectTo} />
           <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                id="remember"
-                name="remember"
-                type="checkbox"
-              />
-              <label
-                className="ml-2 block text-sm text-gray-900"
-                htmlFor="remember"
-              >
-                Remember me
-              </label>
-            </div>
-            <div className="text-center text-sm text-gray-500">
+            <div className="text-center mt-6 text-gray-500">
               Don't have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{ pathname: "/join" }}
-              >
-                Sign up
+              <Link className="text-cyan-900 hover:text-cyan-700 focus:text-cyan-950" to={{ pathname: "/join" }}>
+                Sign Up
               </Link>
             </div>
           </div>
         </Form>
+      </div>
+      <div className="hidden lg:block flex-1">
+        <img
+          src={triangles}
+          alt="Decorative Triangles"
+          className="absolute inset-y-0 right-0 h-full object-cover"
+        />
       </div>
     </div>
   );
