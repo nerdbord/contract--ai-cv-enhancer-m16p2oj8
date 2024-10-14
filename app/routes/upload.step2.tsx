e35354d2cd742ed2b8ca9134d743b1e309970658
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-10-14 21:24:21 +0200

    feat: collect all data - collect data and save in cookie

--- app/routes/upload.step2.tsx
+++ app/routes/upload.step2.tsx
@@ -1,15 +1,52 @@
+import { ActionFunctionArgs } from '@remix-run/node'
+import { redirect, useNavigation } from '@remix-run/react'
 import { useState } from 'react'
 import { Button } from '~/components/ui/button'
 import { Input } from '~/components/ui/input'
 import { Textarea } from '~/components/ui/textarea'
+import { formCookie } from '~/lib/cookies'
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  // Step 1: Get form data
+  const formData = await request.formData()
+  const jobTitle = formData.get('jobTitle') as string
+  const companyName = formData.get('companyName') as string
+  const jobDescription = formData.get('jobDescription') as string
+
+  // Step 2: Retrieve the current cookie value
+  const cookieHeader = request.headers.get('Cookie')
+  const existingData = (await formCookie.parse(cookieHeader)) || {}
+  console.log(existingData)
+
+  // Step 3: Update the cookie with new data
+  const updatedData = {
+    resumeText: existingData, // Keep existing extractedText
+    jobTitle, // Add new data
+    companyName,
+    jobDescription,
+  }
+
+  // Step 4: Serialize and set the updated cookie
+  const updatedCookieHeader = await formCookie.serialize(updatedData)
+
+  // Step 5: Redirect to the next step, with the updated cookie
+  return redirect('/upload/step3', {
+    headers: {
+      'Set-Cookie': updatedCookieHeader,
+    },
+  })
+}
 
 const step2 = () => {
   const [jobTitle, setJobTitle] = useState('')
   const [companyName, setCompanyName] = useState('')
   const [jobDescription, setJobDescription] = useState('')
 
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === 'submitting'
+
   return (
-    <form className="w-full max-w-md space-y-4">
+    <form method="post" className="w-full max-w-md space-y-4">
       <div>
         <label
           htmlFor="jobTitle"
@@ -19,9 +56,11 @@ const step2 = () => {
         </label>
         <Input
           id="jobTitle"
+          name="jobTitle"
           placeholder="Position you apply for. Eg. Senior product designer."
           value={jobTitle}
           onChange={(e) => setJobTitle(e.target.value)}
+          required
         />
       </div>
 
@@ -34,9 +73,11 @@ const step2 = () => {
         </label>
         <Input
           id="companyName"
+          name="companyName"
           placeholder="Name of the company you apply to."
           value={companyName}
           onChange={(e) => setCompanyName(e.target.value)}
+          required
         />
       </div>
 
@@ -49,13 +90,19 @@ const step2 = () => {
         </label>
         <Textarea
           id="jobDescription"
+          name="jobDescription"
           placeholder="Paste the text of job offer description."
           value={jobDescription}
           onChange={(e) => setJobDescription(e.target.value)}
+          required
         />
       </div>
 
-      <Button type="submit" className="w-full">
+      <Button
+        disabled={!jobTitle || !jobDescription || !companyName || isSubmitting}
+        type="submit"
+        className="w-full"
+      >
         Start enhancing my resume
       </Button>
     </form>
--- app/routes/upload.step3.tsx
+++ app/routes/upload.step3.tsx
@@ -1,5 +1,37 @@
+import { LoaderFunctionArgs } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { formCookie } from '~/lib/cookies'
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const cookieHeader = request.headers.get('Cookie')
+  const cookieData = await formCookie.parse(cookieHeader)
+
+  return { cookieData }
+}
+
 const step3 = () => {
-  return <div>ImproveResume</div>
+  // @ts-ignore
+  const { cookieData } = useLoaderData()
+
+  return (
+    <>
+      {cookieData ? (
+        <div>
+          ImproveResume
+          <h6>jobTitle</h6>
+          <p className="text-green-600">{cookieData.jobTitle}</p>
+          <h6>companyName</h6>
+          <p className="text-green-600">{cookieData.companyName}</p>
+          <h6>jobDescription</h6>
+          <p className="text-green-600">{cookieData.jobDescription}</p>
+          <h6>CV</h6>
+          <p className="text-green-600">{cookieData.resumeText}</p>
+        </div>
+      ) : (
+        <div>no cookie</div>
+      )}
+    </>
+  )
 }
 
 export default step3
