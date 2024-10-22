408b38421f09989c34a4f441a9d9edfb4e6a8c79
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-10-22 20:33:56 +0200

    feat: netlify deploy v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { netlifyPlugin } from '@netlify/remix-edge-adapter/plugin'
+import { netlifyPlugin } from '@netlify/remix-adapter/plugin'
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
